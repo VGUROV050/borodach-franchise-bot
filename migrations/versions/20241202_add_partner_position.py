bf111c9d739fc0853b18678245075be901c0d85d
Vladislav Gurov <vgurov050@gmail.com>
2025-12-02 17:42:28 +0400

    fix: correct down_revision in migration

--- migrations/versions/20241202_add_partner_position.py
+++ migrations/versions/20241202_add_partner_position.py
@@ -11,7 +11,7 @@ import sqlalchemy as sa
 
 # revision identifiers, used by Alembic.
 revision = '20241202_position'
-down_revision = '20241201_add_client_stats'
+down_revision = 'add_client_stats'
 branch_labels = None
 depends_on = None
 
